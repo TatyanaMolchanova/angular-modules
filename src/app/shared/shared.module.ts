8752bb5d2feccc0b49203b82e4168babd16753d9
Molchanova Tatyana <mtuspeh@gmail.com>
2020-09-20 11:13:53 +0300

    n commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {ColorDirective} from './color.directive';
+import {PageNamePipe} from './page-name.pipe';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+    declarations: [
+        ColorDirective,
+        PageNamePipe
+    ],
+    imports: [
+        // CommonModule
+    ],
+    exports: [
+        ColorDirective,
+        PageNamePipe,
+        // CommonModule
+    ]
+})
+export class SharedModule {
+}
